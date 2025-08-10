4a29396e34f0bbe3e1c72f5ea7978d3fc557898a
kenzyyasserr <kanzy.yasser.mohammed@gmail.com>
2025-08-11 00:09:05 +0300

    letting the user guess

--- wordle game/game_logic.py
+++ wordle game/game_logic.py
@@ -1,7 +1,20 @@
 # task 6.1: backend
 def guess_word ():
     import random
-    words_5 = ["beach", "ocean", "river", "paint", "happy", "drums", "radio", "tiger", "bread", "pixel", "paint", "drill", "truck", "shirt", "virus", "comet"]
+    words_5 = {"beach", "ocean", "river", "paint", 
+               "happy", "drums", "radio", "tiger", 
+               "bread", "pixel", "paint", "drill", 
+               "truck", "shirt", "virus", "comet"}
     selected_word = random.choice (words_5)
+    
+    user_guess = input("guess the word: ")
+    for n in range(6):
+        if selected_word == selected_word:
+            print ("Congrats! you've guessed the right word in " + n + " attempts")
+        else:
+            print ("That's not correct please try again")
+
+guess_word ()
+
 
 
--- wordle game/main.py
+++ wordle game/main.py
@@ -1,3 +1,4 @@
 print ("Welcome to wordle!")
 print ("A quick heads up, wordle is a game about guessing a 5 letter word in 6 or fewer attempts")
 print ("ARE YOU READY? \nLet's get started!")
+from game_logic import guess_word
