781702074daf56e2842da14e521d90e47e7671b2
kenzyyasserr <kanzy.yasser.mohammed@gmail.com>
2025-08-11 20:54:22 +0300

    Update game_logic.py

--- wordle game/game_logic.py
+++ wordle game/game_logic.py
@@ -1,20 +1,23 @@
 # task 6.1: backend
-def guess_word ():
+def guess_word():
     import random
-    words_5 = {"beach", "ocean", "river", "paint", 
+    words_5 = ["beach", "ocean", "river", "paint", 
                "happy", "drums", "radio", "tiger", 
                "bread", "pixel", "paint", "drill", 
-               "truck", "shirt", "virus", "comet"}
+               "truck", "shirt", "virus", "comet"]
     selected_word = random.choice (words_5)
     
     user_guess = input("guess the word: ")
     for n in range(6):
-        if selected_word == selected_word:
-            print ("Congrats! you've guessed the right word in " + n + " attempts")
+        if user_guess == selected_word:
+            print ("Congrats! you've guessed the right word in" + str(n+1) + " attempts")
+            break
         else:
             print ("That's not correct please try again")
+            user_guess = input("guess the word: ")
+            continue
 
-guess_word ()
+guess_word()
 
 
 
