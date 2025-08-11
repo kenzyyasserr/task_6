9fa9dcd5124d4235cc8e15e8b002a56a8ce7a266
Marwan <138301321+marwanelgharbawy@users.noreply.github.com>
2025-08-11 22:55:12 +0300

    Fix all errors
    Errors include:
    - Making words_5 an array instead of a set
    - Put the input function inside the for loop

--- wordle game/game_logic.py
+++ wordle game/game_logic.py
@@ -1,20 +1,19 @@
 # task 6.1: backend
 def guess_word ():
     import random
-    words_5 = {"beach", "ocean", "river", "paint", 
+    words_5 = ["beach", "ocean", "river", "paint", 
                "happy", "drums", "radio", "tiger", 
                "bread", "pixel", "paint", "drill", 
-               "truck", "shirt", "virus", "comet"}
-    selected_word = random.choice (words_5)
+               "truck", "shirt", "virus", "comet"]
+    selected_word = random.choice(words_5)
     
-    user_guess = input("guess the word: ")
     for n in range(6):
-        if selected_word == selected_word:
-            print ("Congrats! you've guessed the right word in " + n + " attempts")
+        user_guess = input("guess the word: ")
+        if selected_word == user_guess:
+            print ("Congrats! you've guessed the right word in " + str(n) + " attempts")
+            break
         else:
             print ("That's not correct please try again")
+            print ("You have " + str(5 - n) + " attempts left")
 
 guess_word ()
-
-
-
